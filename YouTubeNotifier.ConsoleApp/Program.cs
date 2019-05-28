a44d6aa5c1860afca36f551a3ec01db7458a3a12
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-05-29 00:33:19 +0900

    add console application project

---
+++ YouTubeNotifier.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouTubeNotifier.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
