ef273de8521f62ae835efc1bf6882a20a0ce77db
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-06-23 20:39:57 +0900

    add crawler console project

---
+++ YouTubeNotifier.VTuberRankingCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouTubeNotifier.VTuberRankingCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
