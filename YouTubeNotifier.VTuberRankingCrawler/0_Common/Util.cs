66a12c2105b95005967b9193158a5991399080b2
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-07-14 17:14:52 +0900

    不要になったデバッグコードを削除した

--- YouTubeNotifier.VTuberRankingCrawler/0_Common/Util.cs
+++ YouTubeNotifier.VTuberRankingCrawler/0_Common/Util.cs
@@ -1,79 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace YouTubeNotifier.VTuberRankingCrawler.Common
 {
-    public static class Util
-    {
-        private static readonly string CacheDirectoryName = "Cache";
-
-        public static T Cache<T>(Func<T> dataFecher, string key)
-        {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var cacheDirectory = Path.Combine(currentDirectory, CacheDirectoryName);
-
-            if (!Directory.Exists(cacheDirectory))
-            {
-                Directory.CreateDirectory(cacheDirectory);
-            }
-
-            var filePath = Path.Combine(cacheDirectory, $"{key}.json");
-
-            if (File.Exists(filePath))
-            {
-                var json = File.ReadAllText(filePath);
-                return JsonConvert.DeserializeObject<T>(json);
-            }
-            else
-            {
-                var data = dataFecher();
-
-                var json = JsonConvert.SerializeObject(data);
-                File.WriteAllText(filePath, json);
-
-                return data;
-            }
-        }
-
-        public static async Task<T> Cache<T>(Func<Task<T>> dataFecher, string key)
-        {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var cacheDirectory = Path.Combine(currentDirectory, CacheDirectoryName);
-
-            if (!Directory.Exists(cacheDirectory))
-            {
-                Directory.CreateDirectory(cacheDirectory);
-            }
-
-            var filePath = Path.Combine(cacheDirectory, $"{key}.json");
-
-            if (File.Exists(filePath))
-            {
-                var json = File.ReadAllText(filePath);
-                return JsonConvert.DeserializeObject<T>(json);
-            }
-            else
-            {
-                var data = await dataFecher();
-
-                var json = JsonConvert.SerializeObject(data);
-                File.WriteAllText(filePath, json);
-
-                return data;
-            }
-        }
-
-        public static string JoinString(this IEnumerable<string> source, string separator)
-        {
-            return string.Join(separator, source.ToArray());
-        }
-    }
-
     public static class ConverterExtensions
     {
         public static MemoryStream ToMemoryStream(this string text)
